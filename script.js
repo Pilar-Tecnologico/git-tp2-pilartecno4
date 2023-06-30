071d64bda8c4e1b837b9b2f43ad37cb0c5a6e028
Maxi Ivan <maximilianoivandelafuente@gmail.com>
2023-06-29 23:37:19 -0300

    refactor_code

---
+++ script.js
@@ -0,0 +1,29 @@
+const gameForm = document.getElementById('gameForm');
+const gameName = document.getElementById('gameName');
+const gameStatus = document.getElementById('gameStatus');
+
+gameForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  // Crear una nueva fila y celdas
+  var newRow = document.createElement('tr');
+  var nameCell = document.createElement('td');
+  var statusCell = document.createElement('td');
+
+  // Asignar los valores del formulario a las celdas
+  nameCell.textContent = gameName.value;
+  statusCell.textContent = gameStatus.value;
+
+  // Añadir las celdas a la nueva fila
+  newRow.appendChild(nameCell);
+  newRow.appendChild(statusCell);
+
+  console.log(newRow);
+
+  // Añadir la nueva fila a la tabla
+  document.getElementById('gameList').appendChild(newRow);
+
+  // Limpiar el formulario
+  gameName.value = '';
+  gameStatus.value = 'Planeo jugar';
+});
