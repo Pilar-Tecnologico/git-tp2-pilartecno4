c6f0f9077fc465a2b2cc1f223cf8565c0e7691af
Agustin <agustinbracco174@gmail.com>
2023-06-30 06:09:29 -0300

    se corrigieron errores en el codigo

--- validar.js
+++ validar.js
@@ -3,8 +3,8 @@ const juego = document.getElementById("gameName");
 
 boton.disabled = true;
 
-juego.addEventListener('input', function(event) {
-    if(juego.Value.trim() === '') {
+juego.addEventListener('input', function() {
+    if(juego.value.trim() === '') {
         boton.disabled = true;
     } else {
         boton.disabled = false;
